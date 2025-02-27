5ce860223c03189f9c0b51f9184e8fd9b3f466a4
Odd Stråbø <oddstr13@gmail.com>
2025-02-27 12:01:50 +0100

    Add models  for crew

--- admin.py
+++ admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Crewmember
+
+
+class CrewmemberAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "phone_number"]
+
+
+admin.site.register(Crewmember, CrewmemberAdmin)
--- models.py
+++ models.py
@@ -6,7 +6,28 @@ from .validators import discord_username_validator
 
 class Attendant(models.Model):
     ticket_id = models.CharField(max_length=128, unique=True, null=True, blank=True)
-    nfc_id = models.BinaryField(max_length=7, unique=True, null=False, blank=False)
+    nfc_id = models.CharField(max_length=14, unique=True, null=False, blank=False)
+    is_crew = models.BooleanField(default=False)
+    _is_valid = models.BooleanField(name="is_valid", default=True)
+
+    crewinfo = models.ForeignKey(
+        "Crewmember", on_delete=models.CASCADE, default=None, null=True
+    )
+
+    @property
+    def is_valid(self):
+        # TODO: Validate daily ticket here
+        return self._is_valid
+
+
+class Crews(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Crewmember(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
     discord = models.CharField(
         max_length=32,
         unique=True,
@@ -14,10 +35,11 @@ class Attendant(models.Model):
         blank=True,
         validators=[discord_username_validator],
     )
-    is_crew = models.BooleanField(default=False)
-    _is_valid = models.BooleanField(name="is_valid", default=True)
+    phone_number = models.CharField(max_length=15)
+    crew = models.ForeignKey(Crews, on_delete=models.SET_NULL, null=True)
+    profile_image = models.ImageField(
+        upload_to="profile_images/", blank=True, null=True
+    )
 
-    @property
-    def is_valid(self):
-        # TODO: Validate daily ticket here
-        return self._is_valid
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
